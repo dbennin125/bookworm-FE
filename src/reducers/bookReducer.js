858b926a19544978d556ef6bfa09b66a85135300
Dan B <dbennington125@gmail.com>
2020-08-10 15:08:38 -0700

    store added

---
+++ src/reducers/bookReducer.js
@@ -0,0 +1,12 @@
+export const initialState = {
+  loading: false,
+  bookByID: {},
+  books: [],
+};
+
+export default function reducer(state, action) {
+  switch(action.type) {
+    default: 
+      return state;
+  }
+}
