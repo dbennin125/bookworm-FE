c8ed6457fd046053ad556a9157a89bfe0795147f
Dan B <dbennington125@gmail.com>
2020-08-11 19:22:08 -0700

    dinner break

--- services/auth.js
+++ services/auth.js
@@ -1,25 +1,33 @@
 
+import { post } from './request';
 
 export const fetchSignup = (email, password, userImage) => {
-  return fetch(`${process.env.API_URL}/api/v1/auth/signup`, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    credentials: 'include',
-    body: JSON.stringify({ email, password, userImage })
-  })
-    .then(res => res.json());
+  return post('/api/v1/auth/signup', { email, password, userImage });
 };
 
-export const fetchLogin = (email, password) => {
-  return fetch(`${process.env.API_URL}/api/v1/login`, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    credentials: 'include',
-    body: JSON.stringify({ email, password })
-  })
-    .then(res => res.json());
-};
+export const fetchLogin = (email, password) => 
+  post('/api/v1/auth/login', { email, password });
+
+// export const fetchSignup = (email, password, userImage) => {
+//   return fetch(`${process.env.API_URL}/api/v1/auth/signup`, {
+//     method: 'POST',
+//     headers: {
+//       'Content-Type': 'application/json'
+//     },
+//     credentials: 'include',
+//     body: JSON.stringify({ email, password, userImage })
+//   })
+//     .then(res => res.json());
+// };
+
+// export const fetchLogin = (email, password) => {
+//   return fetch(`${process.env.API_URL}/api/v1/login`, {
+//     method: 'POST',
+//     headers: {
+//       'Content-Type': 'application/json'
+//     },
+//     credentials: 'include',
+//     body: JSON.stringify({ email, password })
+//   })
+//     .then(res => res.json());
+// };
